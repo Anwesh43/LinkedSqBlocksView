2184eb62d4208055564d02f7ac0a6e2dd291434f
Anwesh43 <anweshthecool0@gmail.com>
2019-03-05 01:51:24 +0530

    adding SqBlocksRot class

--- sqblocksrotview/src/main/java/com/anwesh/uiprojects/sqblocksrotview/SqBlocksRotView.kt
+++ sqblocksrotview/src/main/java/com/anwesh/uiprojects/sqblocksrotview/SqBlocksRotView.kt
@@ -159,4 +159,27 @@ class SqBlocksRotView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class SqBlocksRot(var i : Int) {
+        private var dir : Int = 1
+        private val root : SBRNode = SBRNode(0)
+        private var curr : SBRNode = root
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
