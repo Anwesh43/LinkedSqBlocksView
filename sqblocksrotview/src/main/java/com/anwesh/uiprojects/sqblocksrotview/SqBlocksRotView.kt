adbc3f0ed0e4426d200c12a494147e430da89bd2
Anwesh43 <anweshthecool0@gmail.com>
2019-03-05 01:42:08 +0530

    adding SqBlocksRotView class

--- sqblocksrotview/src/main/java/com/anwesh/uiprojects/sqblocksrotview/SqBlocksRotView.kt
+++ sqblocksrotview/src/main/java/com/anwesh/uiprojects/sqblocksrotview/SqBlocksRotView.kt
@@ -50,3 +50,21 @@ fun Canvas.drawSBRNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class SqBlocksRotView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
